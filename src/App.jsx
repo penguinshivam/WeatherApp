86786a7eb652fc44347c81addfe800e6f253fca6
penguinshivam <shivaxagarwal@gmail.com>
2024-11-03 01:21:15 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import './App.css'
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SearchBox from './SearchBox';
+import InfoBox from './InfoBox';
+import WeatherApp from './WeatherApp';
+
+function App() {
+
+  return (
+    <>
+    {/* <SearchBox/>
+    <InfoBox/> */}
+    <WeatherApp/>
+    </>
+  )
+}
+
+export default App
